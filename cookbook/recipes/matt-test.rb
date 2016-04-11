e47d9e0850c06cae1aa23b20e6832fec0887f74f
mineer22 <mineer22@users.noreply.github.com>
2016-04-11 12:23:16 +0200

    Update matt-test.rb

--- cookbook/recipes/matt-test.rb
+++ cookbook/recipes/matt-test.rb
@@ -1,4 +1,4 @@
 # Install the certificate into the Personal store under Computer Certificates
-windows_certificate '#{node['certpath']}' do
-    pfx_password '#{node['pfxpassword']}'
+windows_certificate node['certpath'] do
+    pfx_password node['pfxpassword']
 end
